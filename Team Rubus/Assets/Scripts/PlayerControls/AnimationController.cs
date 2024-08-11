c785ba48be91b203e9709124efb60f42b47913cd
AbramBuck <82072155+AbramBuck@users.noreply.github.com>
2024-08-11 18:24:58 -0500

    Animations Completed!

--- Team Rubus/Assets/Scripts/PlayerControls/AnimationController.cs
+++ Team Rubus/Assets/Scripts/PlayerControls/AnimationController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,32 +9,24 @@ public class AnimationController : MonoBehaviour
     public Animator anim;
 
 
-    Rigidbody rb;
-
-    void Awake()
+    void Start()
     {
-        rb = GetComponentInParent<Rigidbody>();
 
-        if (spriteRenderer == null || anim == null)
-        {
-            Debug.LogError("SpriteRenderer or Animator is not assigned!");
-        }
     }
 
     void Update()
     {
-        //if (rb == null) return; // Exit if Rigidbody is not found
-
-        float speedX = rb.velocity.x;
-        float speedY = rb.velocity.y;
 
         if (Input.GetKey(KeyCode.A))
         {
             spriteRenderer.flipX = true;
             anim.SetBool("Run", true);
-        } else if ((Input.GetKeyUp(KeyCode.A)))
+            Debug.Log("I can run");
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
         {
             anim.SetBool("Run", false);
+            Debug.Log("No run");
         }
 
 
@@ -42,54 +35,20 @@ public class AnimationController : MonoBehaviour
         {
             spriteRenderer.flipX = false;
             anim.SetBool("Run", true);
-        } else if (Input.GetKeyUp(KeyCode.D))
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
         {
             anim.SetBool("Run", false);
         }
 
-
-
-
-        /*
-        // Check if the Rigidbody is moving on the x axis
-        if (Mathf.Abs(speedX) > Mathf.Epsilon)
+        if (Input.GetKey(KeyCode.Space))
         {
-            if (speedX > 0)
-            {
-                Debug.Log("Rigidbody is moving in the +x direction at speed: " + speedX);
-                spriteRenderer.flipX = false;
-                //anim.SetBool("Run", true);
-                anim.Play("run");
-            }
-            else if (speedX < 0)
-            {
-                Debug.Log("Rigidbody is moving in the -x direction at speed: " + speedX);
-                spriteRenderer.flipX = true;
-                //anim.SetBool("Run", true);
-                anim.Play("run");
-            }
+            anim.SetBool("Jump", true);
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            //anim.SetBool("Run", false); // Stop running if no movement on x-axis
-            anim.Play("idle");
+            anim.SetBool("Jump", false);
         }
-
-        // Check if the Rigidbody is moving on the y axis
-        if (Mathf.Abs(speedY) > Mathf.Epsilon)
-        {
-            if (speedY > 0)
-            {
-                Debug.Log("Rigidbody is moving in the +y direction at speed: " + speedY);
-            }
-            else
-            {
-                Debug.Log("Rigidbody is moving in the -y direction at speed: " + speedY);
-            }
-        }
-        else
-        {
-            Debug.Log("Rigidbody is not moving on the y axis.");
-        }*/
     }
-}
+   }
+
